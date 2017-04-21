5226793f5cb84ff61158e1a974cae870de35eaaa
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-04-21 11:31:24 -0500

    End of April 21 Class

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,46 @@
+class GameController < ApplicationController
+  def play_rock
+    moves = ["rock","paper","scissors"]
+
+    @computer_move = moves.sample
+    if @computer_move == "rock"
+      @outcome = "tied."
+    elsif @computer_move == "paper"
+      @outcome = "lost."
+    else
+      @outcome = "won."
+    end
+
+    render("game/play_rock.html.erb")
+  end
+
+  def play_paper
+    moves = ["rock","paper","scissors"]
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "won."
+    elsif @computer_move == "paper"
+      @outcome = "tied."
+    else
+      @outcome = "lost."
+    end
+
+    render("game/play_paper.html.erb")
+  end
+
+  def play_scissors
+    moves = ["rock","paper","scissors"]
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @outcome = "lost."
+    elsif @computer_move == "paper"
+      @outcome = "won."
+    else
+      @outcome = "tied."
+    end
+
+    render("game/play_scissors.html.erb")
+  end
+end
